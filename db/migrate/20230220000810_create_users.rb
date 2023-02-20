0b2cf278e0054368461f453169889c4896aa9610
tomasphn <combomeal12@gmail.com>
2023-02-19 16:11:03 -0800

    initial commit

---
+++ db/migrate/20230220000810_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+    end
+  end
+end
