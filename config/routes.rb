ce87f2f48334a65559fd5720f0e44664538038f1
tomasphn <combomeal12@gmail.com>
2023-02-19 17:07:33 -0800

    add error messages to edit and new user view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :edit, :show]
+  resources :users, only: [:new, :create, :edit, :update, :show]
   root to: "users#new"
 end
