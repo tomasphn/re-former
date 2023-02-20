f0cff6780195925a52ea5a6f1b52e9755348702d
tomasphn <combomeal12@gmail.com>
2023-02-19 17:02:27 -0800

    add edit, update, and show routes, views,  actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,28 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id]) 
   end
 
   private
